6b98655590061325da5934b8547513e1eb5f27cd
BurcisWolf <furcis.burcis@gmail.com>
2026-05-12 13:58:15 +0200

    Transaction interface and TransactionType created. Working on Budget class

---
+++ Monthly Budget Tracker/src/models/Budget.ts
@@ -0,0 +1,23 @@
+﻿import type {Transaction} from "@/models/Transaction.ts";
+
+class Budget {
+    private transactions: Transaction[];
+    private name: string;
+
+    constructor(transactions: Transaction[] = [], name: string){
+        this.transactions = transactions;
+        this.name = name;   
+    }
+
+    getAllTransactions() : Transaction[] {
+        return [...this.transactions];
+    }
+    
+    getName(): string {
+        return this.name;
+    }
+    
+    changeName(name: string) : void {
+        this.name = name;
+    }
+}
---
+++ Monthly Budget Tracker/src/models/Transaction.ts
@@ -0,0 +1,12 @@
+﻿import type {TransactionType} from "@/models/TransactionType.ts";
+
+export interface Transaction {
+    id: string;
+    title: string;
+    amount: number;
+    type: TransactionType;
+    categoryId: string;
+    date: string;
+    note?: string;
+    createdAt: string;
+}
