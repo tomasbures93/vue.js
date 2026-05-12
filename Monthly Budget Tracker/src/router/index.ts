f21ec24b2f78b37a8bfb14dd7112b9dddf379df1
BurcisWolf <furcis.burcis@gmail.com>
2026-05-12 09:24:30 +0200

    Created routes + tested

--- Monthly Budget Tracker/src/router/index.ts
+++ Monthly Budget Tracker/src/router/index.ts
@@ -1,26 +1,50 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import AboutView from '../views/AboutView.vue'
+import DashboardView from "@/views/DashboardView.vue";
+import TransactionsView from "@/views/TransactionsView.vue";
+import AddTransactionView from "@/views/AddTransactionView.vue";
+import CategoriesView from "@/views/CategoriesView.vue";
+import SettingsView from "@/views/SettingsView.vue";
+import NotFound from "@/views/NotFound.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'dashboard',
+      component: DashboardView,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/transactions',
+      name: 'transactions',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       
       // Two options bow to import components
       // component: () => import('../views/AboutView.vue'),
-      component: AboutView,
+      component: TransactionsView,
     },
+    {
+      path: '/transactions/new',
+      name: 'NewTransaction',
+      component: AddTransactionView,
+    },
+    {
+      path: '/categories',
+      name: 'categories',
+      component: CategoriesView,
+    },
+    {
+      path: '/settings',
+      name: 'settings',
+      component: SettingsView,
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'NotFound',
+      component: NotFound,
+    }
   ],
 })
 
